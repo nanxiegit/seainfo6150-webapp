e0b026005f44bcc3782c937c5635ead8e48d5fd2
Nan  Xie <xie.n@northeastern.edu>
2021-02-28 20:28:03 -0800

    assignment javascript

--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -5,6 +5,15 @@ const DynamicArticle = (props) => {
   return (
     <article>
       {/* add extra html with javascript attributes here */}
+      <header>
+        <h1>You can take a pie on a plane</h1>
+        <address>
+          by Cathryn Lindsey (
+          <a href="mailto:cathryn.lindsey@nytimes.com">cathryn.lindsey@nytimes.com</a>)
+          <br />
+        </address>
+        <time datetime="2018-11-21 14:57:55">November 21st 2018, 2:57 pm </time>
+      </header>
       <HTMLText text={props.article.text} />
     </article>
   );
